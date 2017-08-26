7d5820d8786c188c286d0c0cd5cf0c28fda2bffe
notgoodalso <dzhax@hotmail.com>
2017-08-26 00:38:27 -0400

    Upload
    Initial upload

---
+++ minimap.js
@@ -0,0 +1,115 @@
+﻿///<reference path="/alt1lib.js">
+///<reference path="/runeappslib.js">
+///<reference path="/imagedetect.js">
+///<reference path="/apps/alt1/alt1doc.js">
+"use strict";
+
+MinimapReader.homeportbutton = null; ImageData.fromBase64(function (i) { MinimapReader.homeportbutton = i; }, "iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAIAAAC0tAIdAAAA70lEQVQoU4WSoW4CQRRF5ydQdUgcAsEaEpIK1P4AAdUQXAXB4FuNgiCKJdUVdYsihKQG+IKmAgFyg8D0TO7mMRkamhzx3n3nzWwy694+fz62+XTy9dx6fWoMIwgZIaC16333vjr2B3OqOyCgseB6nVla64bIiEI0bnBqHitpfr6AjUEJI7WjceZterNV3yZoXOiiWVJuGqENLBS2zaoPiVQKJdS2cLV33/mfxLZOiiRDtxU2VWhXSlUjtIW36Zmt9ycIbUtswUmF5eYAmim0RKH/EjXA24IGsi0RvL9/HevvgOb/k5fF9t8FBLTROPsFtJgUMNwTN50AAAAASUVORK5CYII=");
+
+ImageData.fromBase64(i=>MinimapReader.energies.botrun.img = i, "iVBORw0KGgoAAAANSUhEUgAAABYAAAAKCAYAAACwoK7bAAACbElEQVQ4T32Sy2sTURTGZ2uazKMZiJKkpknaPE1aFYogbgQ3RfCxkvqgiqb2EZNi2tiaTOrENslQsZWKQVAXvmp8QWzTdiEIrqSlIRWsYuvChRvxX/g8d8BSRV183Ln3fPfHd84dTmupRz5cD3WHhIRXQO7INnxZaMSPNw2oxIyoZXjEdkpI+kVkQxLS5Ov2yiif2qJ7lHazXhulGuMUiMeY3GhAQsYr4qKbR39Ywvd3XqyM8FiOGlBN8UjtE/TaFfLk/BLUoIQ47e8fr8PXmxLKIyH00H6oWdA5WZ+IAnm4FJn7qXDeb8LarEOH1hQe2mEZEYcJg03CBrRAIcZoHSZIl1PA9BkDdedAh82oe4un3XpHGiXnIkEZ1XtWfCuZsdhjwNQJM87SpbiL0noEXKUEDJonKAOzlaVKuAXEqMMPEyJKigXvH1rx5JwB6d0ilDYLuL727VgZF7HUa8BJuxFRJ4/LBFT/AG4WO1c8IvrIWx4wYXVSxOM99D5sbCEZuY4mcMW0G88HJSynefQ6TTqUtfs34GaxTpI0Ju2QjEeddeiicUb9ZqSOOlGZ8oN7+8CKO6lGvL5mRLzNBjVAw6eZapTof8o3i1AJPEzAJIWJ77JCORbAqxs+fKw0gAPALU7bUJr0Itftw6UDDmTDMjR6II0l+ocKpDwpE6Lfje6MXwhg4W4QnwnKmDqYaXXGgqWnDszc8uB6pAVD+10Y2GtHvlVGgUazAWRjojNWYx7mnS16UH3hwNr8Vh36G5hpfc6K9XkLai9dmLvtw7OxMCbiIaidQSQOtupi3+yM1ZiHedmdTxU7IX6xwP0EFsu8kp8kDHUAAAAASUVORK5CYII=");
+ImageData.fromBase64(i=>MinimapReader.energies.toprun.img = i, "iVBORw0KGgoAAAANSUhEUgAAABYAAAAKCAYAAACwoK7bAAACWUlEQVQ4T12SXUhTYRjHdxvYxfIqaWfn+3ub07lJow8JvegmdEUZFQZWSIGJ0IUQFNSIgYHBxAvrouiqgiiKCCrrLiIIKcEIdRM/qjkvKjQI/j3Pa1utiz/nPed9/7/n/zzvCQCo0dL0WayXTuJXuRc/FruxMnMASx8zWJjqEuI1f+M9PsNn2fM/p7ooTPZj7esJfF88ivkPGdwe24Xjh3zsSGrwbBmGJkFTQrD1EFrjMno6bdzM70TxfUZ42MuMGnBpuhdry0doYx/OD7SiOapA18JVuY4Jy9TgO2Gc64uhsz0MSWqASoXivoKh/pTwMoNZArz66Ri+LRzEg1u7kU6oAuS5Vo0c24CmyZh6pODnrCk0eCoG01DhUFH2bCcvM5jFzMDq3H6MXE7BMWUB4XQMsi0dUd8S8l1DmFcmJJTf2ii/lHBn1IOmyrDonG0ZwuNZYVzLJsHMQP5KCqa+kZKhDIp4Os1VgaHLlEqGrkqQQg14NxzE8r16ofvZOtimQuPRkRvyYNKoGM6s4YtJBHy6mErLsahNcB1v7qpYnzHx+bWN6ScqHt7wMZZNYPaZIqDjA1uosArLUHDpTKMYzd42eqf0ghOxEOBFU6MrtNG2JYCTV4Mojv5NyO1XxjAxson+kLC40K42BV+eS+juUKhTE/GYK+BVcDzmkHy0p3UUntZjT4uCdFxFT8dWFPNBzFMRASdxoesXtkEJh3D6sIHSCwmeGKNZDRmoLFqaY9SCi/FcBI9zdXBtk0R/A11QgcD/Jucnt58d1MXIXvVtprQ2Ek3RP2AXvwFBOdopSrlbYgAAAABJRU5ErkJggg==");
+ImageData.fromBase64(i=>MinimapReader.energies.botwalk.img = i, "iVBORw0KGgoAAAANSUhEUgAAABYAAAAKCAYAAACwoK7bAAABYUlEQVQ4T52QS0sCURiGzz+IGqNVLaICnRCKxGvo1EiUgUQWZdSUBYKBKG1sI+KlsU5EGVK0iKAoiKCgH/g23xGnGZyFuHhg5jvv+5wLu56XYOVSmcRPK4v3eholTQHPraDhG7NlmnMSCik/vlsHIm9d68Ku5GEQ9FP3uvD7lEflWBG0c6uo+cfNcDdL3KV8eDzfxvPJkjmzZkxxQx7BV1MDzyfAs3HoqtsMOdE0qO4E8KkfmrOb2BTqhoe+hfi1oOKtvAWeUXC77oXu6RU5UfOO4uFswzj1Mj7Ku6hkYqjNdk4txC/FBKrpYE+xH/h+RDwJ9fXwhDlnasgNvrmA9umardA36gz4XggXlluSk0UXg4hHPLjXosY7WwoDQlJyMgAsoCpiUPJI4NNDA0FdcpCLnEJMxJJJhIM+HMkusbNT2QnKUoe6ATUhpDZxF9qRQrQ7FZxuQTNao0xH2DnlP2B/M7mmqCM7tWUAAAAASUVORK5CYII=");
+ImageData.fromBase64(i=>MinimapReader.energies.topwalk.img = i, "iVBORw0KGgoAAAANSUhEUgAAABYAAAAKCAYAAACwoK7bAAAB30lEQVQ4T2WS204TURSG5x1owAOHqHVKWwotbZ1MaUsjJKLU1A5WA2oNwRCLFSFtgxgMIQESg5SCChjjjaQ18RC9MOEFjK/1O/8iM+m0F99kzd7/+vaelVEAOPhcb2B9bxcLxQWkMhlo0RDcV/rQdeGiwJpr3GOGWfa0etqExlRWmiP9LuiXzyGudmPoai887j6BdcLTjeSlTskwO5lOtx0gj71aDbNzTyQU87kQVXuktvD7PGd4VRg3NUSDqr0XNrO62cOaDrpEzJPGryfh7z+PmHk7h6iF99tzOK2v4N/PCm7diNvr7NHNAwZMB110KqMJHRFvJ/zuXoeEDAd9Dr5VH+L3xyX8OshjeyXXlg+YYwp7XAiPpKBwTpydtUlBaMgrtIrfrRk4Xr2HxuYMXhUNe/1xbszupys50AFl0DylWUq2Vmfw56SC+ofn+PT2Kd68zmNt+T5OqgURl59N4FokKNnpbErGo0UDtifoV6E0Cwl/pcbRIg4qBg5f5uR2hJ9vjaFaykqOjMaG8WW3gIQWsB10OsQM6loIP2qPENfDUhsTI+Yhd7GzOClSi9L8HcnnMnF83XkgteUhSrOUFGdvY6OQtt/JfnnKcXOOg5//Yj6Dv9/LKOXH7eyZz4f/f7KmZoAX4KUAAAAASUVORK5CYII=");
+MinimapReader.energies = {
+	botrun: { x: 35, y: -28, img: null },
+	toprun: { x: 35, y: -9, img: null },
+	topwalk: { x: 35, y: -9, img: null },
+	botwalk: { x: 35, y: -28, img: null },
+};
+
+
+function MinimapReader() {
+	this.pos = {};
+
+	this.find = function (img) {
+		if (!img) { img = a1lib.bindfullrs(); }
+		if (!img) { return null; }
+		var homeport = a1lib.findsubimg(img, MinimapReader.homeportbutton);
+		if (homeport.length == 0) { return null; }
+		var botleft = { x: homeport[0].x - 17, y: homeport[0].y + 33 };
+
+		var area = { x: homeport[0].x - 17, y: homeport[0].y - 700 + 33, w: 700, h: 700 };
+		if (area.y < 0) { area.h += area.y; area.y = 0; }
+		if (area.x + area.w >= img.width) { area.w = img.width - area.x - 1; }
+
+		var topright = null;
+		for (var a in MinimapReader.energies) {
+			var pos = a1lib.findsubimg(img, MinimapReader.energies[a].img, null, area.x, area.y, area.w, area.h);
+			if (pos.length != 0) {
+				qw("found energy", a);
+				topright = { x: pos[0].x + MinimapReader.energies[a].x, y: pos[0].y + MinimapReader.energies[a].y };
+				break;
+			}
+		}
+		if (!topright) { return null; }
+
+		var w = topright.x - botleft.x;
+		var h = botleft.y - topright.y
+		this.pos = {
+			x: botleft.x,
+			y: topright.y,
+			w: w,
+			h: h,
+			centerx: w / 2,
+			centery: h / 2,
+		};
+		return this.pos;
+	}
+
+	this.readCompass=function(img) {
+		if (!this.pos) { return null; }
+		var buf;
+		if (img) { buf = img.toData(this.pos.x, this.pos.y, 40, 40); }
+		else { buf = a1lib.getregion(this.pos.x, this.pos.y, 40, 40); }
+
+		var r = 11;
+		var buf2 = buf.clone(new Rect(0, 0, buf.width, buf.height));
+		var centerx = 22;
+		var centery = 22;
+
+		var sx1 = 0, sy1 = 0, m1 = 0;
+		var sx2 = 0, sy2 = 0, m2 = 0;
+		for (var x = round(centerx) - r; x <= round(centerx) + r; x++) {
+			for (var y = round(centery) - r; y <= round(centery) + r; y++) {
+				var i = 4 * x + 4 * buf.width * y;
+				var dx = x - centerx;
+				var dy = y - centery;
+				if (dx * dx + dy * dy > r * r) { buf.data[i] = buf.data[i + 1] = buf.data[i + 2] = 0; continue; }
+
+				var rating1 = buf.data[i] - buf.data[i + 1];
+				sx1 += dx * rating1;
+				sy1 += dy * rating1;
+				m1 += rating1;
+				var rating2 = Math.max(0, buf.data[i] + buf.data[i + 1] + buf.data[i + 2] - 300);
+				sx2 += dx * rating2;
+				sy2 += dy * rating2;
+				m2 += rating2;
+				if (isNaN(m2)) { debugger; }
+				if (isNaN(m1)) { debugger; }
+
+				buf.data[i] = buf.data[i + 1] = buf.data[i + 2] = rating1;
+				buf2.data[i] = buf2.data[i + 1] = buf2.data[i + 2] = rating2;
+			}
+		}
+		if (m1 == 0 || m2 == 0) { return null; }
+
+		var mx1 = sx1 / m1;
+		var my1 = sy1 / m1;
+		var mx2 = sx2 / m2;
+		var my2 = sy2 / m2;
+
+		var dir = Math.atan2((my2 - my1), (mx2 - mx1));
+		dir += -1 / 180 * Math.PI;
+		dir = (Math.PI + dir) % (Math.PI * 2);
+		return dir;
+	}
+
+	this.readEnergy = function (img) {
+		if (!this.pos) { return null; }
+		if (!img) { img = a1lib.bindregion(this.pos.x + this.pos.w - 50, this.pos.y + 5, 50, 40); }
+		var str = alt1.bindReadColorString(img.handle, "chat", a1lib.mixcolor(255, 255, 255), this.pos.x + this.pos.w - 27 - img.x, this.pos.y + 23 - img.y);
+		var m = str.match(/(\d{1,3})%/);
+		if (!m) { return null; }
+		return +m[1];
+	}
+}
---
+++ script.js
@@ -0,0 +1,280 @@
+// bootstrap qw from runeappslib.js since minimap.js uses it
+qw = function(){};
+
+var playerDot = null;
+var itemDot = null;
+var npcDot = null;
+ImageData.fromBase64(function (result) { 
+    playerDot = result; 
+}, "iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAYAAAC09K7GAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAyJpVFh0WE1MOmNvbS5hZG9iZS54bXAAAAAAADw/eHBhY2tldCBiZWdpbj0i77u/IiBpZD0iVzVNME1wQ2VoaUh6cmVTek5UY3prYzlkIj8+IDx4OnhtcG1ldGEgeG1sbnM6eD0iYWRvYmU6bnM6bWV0YS8iIHg6eG1wdGs9IkFkb2JlIFhNUCBDb3JlIDUuMy1jMDExIDY2LjE0NTY2MSwgMjAxMi8wMi8wNi0xNDo1NjoyNyAgICAgICAgIj4gPHJkZjpSREYgeG1sbnM6cmRmPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5LzAyLzIyLXJkZi1zeW50YXgtbnMjIj4gPHJkZjpEZXNjcmlwdGlvbiByZGY6YWJvdXQ9IiIgeG1sbnM6eG1wPSJodHRwOi8vbnMuYWRvYmUuY29tL3hhcC8xLjAvIiB4bWxuczp4bXBNTT0iaHR0cDovL25zLmFkb2JlLmNvbS94YXAvMS4wL21tLyIgeG1sbnM6c3RSZWY9Imh0dHA6Ly9ucy5hZG9iZS5jb20veGFwLzEuMC9zVHlwZS9SZXNvdXJjZVJlZiMiIHhtcDpDcmVhdG9yVG9vbD0iQWRvYmUgUGhvdG9zaG9wIENTNiAoV2luZG93cykiIHhtcE1NOkluc3RhbmNlSUQ9InhtcC5paWQ6QjQ4QTM1NUY4OUZBMTFFNzkyQjlGNjQ3NkUwM0EzREEiIHhtcE1NOkRvY3VtZW50SUQ9InhtcC5kaWQ6QjQ4QTM1NjA4OUZBMTFFNzkyQjlGNjQ3NkUwM0EzREEiPiA8eG1wTU06RGVyaXZlZEZyb20gc3RSZWY6aW5zdGFuY2VJRD0ieG1wLmlpZDpCNDhBMzU1RDg5RkExMUU3OTJCOUY2NDc2RTAzQTNEQSIgc3RSZWY6ZG9jdW1lbnRJRD0ieG1wLmRpZDpCNDhBMzU1RTg5RkExMUU3OTJCOUY2NDc2RTAzQTNEQSIvPiA8L3JkZjpEZXNjcmlwdGlvbj4gPC9yZGY6UkRGPiA8L3g6eG1wbWV0YT4gPD94cGFja2V0IGVuZD0iciI/PiMMl8AAAAAuSURBVHjaYvz//z8DEIAJIGBk/A8V+fHjB8OL5y+AUkD+54+f/9+/ex8sBxBgACzPGwWqEqJHAAAAAElFTkSuQmCC");
+ImageData.fromBase64(function (result) { 
+    itemDot = result; 
+}, "iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAYAAAC09K7GAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAyJpVFh0WE1MOmNvbS5hZG9iZS54bXAAAAAAADw/eHBhY2tldCBiZWdpbj0i77u/IiBpZD0iVzVNME1wQ2VoaUh6cmVTek5UY3prYzlkIj8+IDx4OnhtcG1ldGEgeG1sbnM6eD0iYWRvYmU6bnM6bWV0YS8iIHg6eG1wdGs9IkFkb2JlIFhNUCBDb3JlIDUuMy1jMDExIDY2LjE0NTY2MSwgMjAxMi8wMi8wNi0xNDo1NjoyNyAgICAgICAgIj4gPHJkZjpSREYgeG1sbnM6cmRmPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5LzAyLzIyLXJkZi1zeW50YXgtbnMjIj4gPHJkZjpEZXNjcmlwdGlvbiByZGY6YWJvdXQ9IiIgeG1sbnM6eG1wPSJodHRwOi8vbnMuYWRvYmUuY29tL3hhcC8xLjAvIiB4bWxuczp4bXBNTT0iaHR0cDovL25zLmFkb2JlLmNvbS94YXAvMS4wL21tLyIgeG1sbnM6c3RSZWY9Imh0dHA6Ly9ucy5hZG9iZS5jb20veGFwLzEuMC9zVHlwZS9SZXNvdXJjZVJlZiMiIHhtcDpDcmVhdG9yVG9vbD0iQWRvYmUgUGhvdG9zaG9wIENTNiAoV2luZG93cykiIHhtcE1NOkluc3RhbmNlSUQ9InhtcC5paWQ6NkFGMjlDRjc4NkQwMTFFN0I2MkFFMkVDQkVBNTFCM0IiIHhtcE1NOkRvY3VtZW50SUQ9InhtcC5kaWQ6NkFGMjlDRjg4NkQwMTFFN0I2MkFFMkVDQkVBNTFCM0IiPiA8eG1wTU06RGVyaXZlZEZyb20gc3RSZWY6aW5zdGFuY2VJRD0ieG1wLmlpZDo2QUYyOUNGNTg2RDAxMUU3QjYyQUUyRUNCRUE1MUIzQiIgc3RSZWY6ZG9jdW1lbnRJRD0ieG1wLmRpZDo2QUYyOUNGNjg2RDAxMUU3QjYyQUUyRUNCRUE1MUIzQiIvPiA8L3JkZjpEZXNjcmlwdGlvbj4gPC9yZGY6UkRGPiA8L3g6eG1wbWV0YT4gPD94cGFja2V0IGVuZD0iciI/PnRBPSIAAAAySURBVHjaYvj//z/Df22t//85OP6D2Az/FeT//xcV+/+DheX/AyCXAST6EsoBsQECDADnBRxpBbwm9gAAAABJRU5ErkJggg==");
+ImageData.fromBase64(function (result) { 
+    npcDot = result; 
+}, "iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAYAAAC09K7GAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAyJpVFh0WE1MOmNvbS5hZG9iZS54bXAAAAAAADw/eHBhY2tldCBiZWdpbj0i77u/IiBpZD0iVzVNME1wQ2VoaUh6cmVTek5UY3prYzlkIj8+IDx4OnhtcG1ldGEgeG1sbnM6eD0iYWRvYmU6bnM6bWV0YS8iIHg6eG1wdGs9IkFkb2JlIFhNUCBDb3JlIDUuMy1jMDExIDY2LjE0NTY2MSwgMjAxMi8wMi8wNi0xNDo1NjoyNyAgICAgICAgIj4gPHJkZjpSREYgeG1sbnM6cmRmPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5LzAyLzIyLXJkZi1zeW50YXgtbnMjIj4gPHJkZjpEZXNjcmlwdGlvbiByZGY6YWJvdXQ9IiIgeG1sbnM6eG1wPSJodHRwOi8vbnMuYWRvYmUuY29tL3hhcC8xLjAvIiB4bWxuczp4bXBNTT0iaHR0cDovL25zLmFkb2JlLmNvbS94YXAvMS4wL21tLyIgeG1sbnM6c3RSZWY9Imh0dHA6Ly9ucy5hZG9iZS5jb20veGFwLzEuMC9zVHlwZS9SZXNvdXJjZVJlZiMiIHhtcDpDcmVhdG9yVG9vbD0iQWRvYmUgUGhvdG9zaG9wIENTNiAoV2luZG93cykiIHhtcE1NOkluc3RhbmNlSUQ9InhtcC5paWQ6RUQ3MDdGRUQ4NkNGMTFFN0JGQkRDNTVCRUEwQUZDQ0QiIHhtcE1NOkRvY3VtZW50SUQ9InhtcC5kaWQ6RUQ3MDdGRUU4NkNGMTFFN0JGQkRDNTVCRUEwQUZDQ0QiPiA8eG1wTU06RGVyaXZlZEZyb20gc3RSZWY6aW5zdGFuY2VJRD0ieG1wLmlpZDpFRDcwN0ZFQjg2Q0YxMUU3QkZCREM1NUJFQTBBRkNDRCIgc3RSZWY6ZG9jdW1lbnRJRD0ieG1wLmRpZDpFRDcwN0ZFQzg2Q0YxMUU3QkZCREM1NUJFQTBBRkNDRCIvPiA8L3JkZjpEZXNjcmlwdGlvbj4gPC9yZGY6UkRGPiA8L3g6eG1wbWV0YT4gPD94cGFja2V0IGVuZD0iciI/PioW1V0AAAA2SURBVHjaYvr//z/D79/s/0EYxGb584fj/58/PxlA4PYdxv9MLCw/GN++Y2AAYU2N/4wAAQYAG9MaqlrUvYgAAAAASUVORK5CYII=");
+
+
+var minimapReader = new MinimapReader();
+var minimap = null;
+var Status = {
+    STARTING: {
+        id: "STARTING",
+        text: "Starting...",
+        class: "paused"
+    },
+    PAUSED: {
+        id: "PAUSED",
+        text: "Paused",
+        class: "paused"
+    },
+    RUNNING: {
+        id: "RUNNING",
+        text: "OK",
+        class: "ok"
+    },
+    ALERT: {
+    	text: "Something Detected!",
+    	class: "alert",
+    	p: {
+	        id: "ALERT",
+	        text: "PLAYERS DETECTED!",
+	        class: "alert",
+	        status: "players"
+	    },
+	    n: {
+	        id: "ALERT",
+	        text: "NPC DETECTED!",
+	        class: "alert",
+	        status: "npcs"
+
+    	},
+    	i: {
+	        id: "ALERT",
+	        text: "Item DETECTED!",
+	        class: "alert",
+	        status: "items"
+
+	    }
+    },
+};
+var settings = {
+	sText: "Starting up...",
+	status: Status.PAUSED,
+	npcs: false,
+	items: false,
+	players: false,
+	draw: false,
+	dSize: 5,
+	fRefresh: 500,
+	mRefresh: 10000
+};
+
+var statusText = null;
+var container = null;
+var stateButton = null;
+
+function start() {
+	loadStorage();
+	statusText = document.getElementById("status");
+    if (!window.alt1) {
+        statusText.innerText = "Alt1 not detected!";
+        return false;
+    }
+    
+    container = document.getElementById("container");
+    stateButton = document.getElementById("state-button");
+    
+    setStatus(Status.STARTING);
+    findMinimap();
+	mapInterval();
+	searchInterval();
+}
+
+function changeState() {
+    if (settings.status.id != Status.PAUSED.id) {
+        setStatus(Status.PAUSED);
+        elid(Status.ALERT.n.status).innerText = '';
+	    elid(Status.ALERT.i.status).innerText = '';
+	    elid(Status.ALERT.p.status).innerText = '';
+    } else {
+        setStatus(Status.RUNNING);
+    }
+    updateMainStatus();
+}
+var prevSearch = null;
+var prevMap = null;
+function mapInterval() {
+	if(prevMap) {
+		clearInterval(prevMap);
+	}
+	prevMap = setInterval(function() {
+        	findMinimap();
+    	}, settings.mRefresh);
+
+}
+function searchInterval() {
+	if(prevSearch) {
+        clearInterval(prevSearch);
+    }
+    prevSearch = setInterval(function() {
+    	if (!minimap || settings.status.id == Status.PAUSED.id) {
+    		setStatusText("Paused");
+    		return;
+    	}    
+        var minimapRegion = a1lib.getregion(minimap.x, minimap.y, minimap.w, minimap.h);
+        var alert = false;
+        if(settings.players) {
+	        var players = find(minimapRegion, playerDot);
+	        if(settings.draw)
+	        for( var a in players ) {
+	        	alt1.overLayRect(a1lib.mixcolor(255, 0, 0), minimap.x + players[a].x, minimap.y + players[a].y, 5, 5, 100, settings.dSize);
+	        }
+	        if(players.length > 0) alert = true;
+	        setStatus(players.length > 0 ? Status.ALERT.p : Status.RUNNING, players.length);
+	    }
+        if(settings.npcs) {
+	        var npcs = find(minimapRegion, npcDot);
+	        if(settings.draw)
+	        for( var a in npcs) {
+	        	alt1.overLayRect(a1lib.mixcolor(255, 0, 0), minimap.x + npcs[a].x, minimap.y + npcs[a].y, 5, 5, 100,settings.dSize);
+	        }
+	        if(npcs.length > 0) alert = true;
+	        setStatus(npcs.length > 0 ? Status.ALERT.n : Status.RUNNING, npcs.length);
+	    }
+        if(settings.items) {
+	        var items = find(minimapRegion, itemDot);
+	        if(settings.draw)
+	        for( var a in items ) {
+	        	alt1.overLayRect(a1lib.mixcolor(255, 0, 0), minimap.x + items[a].x, minimap.y + items[a].y, 5, 5, 100, settings.dSize);
+	        }
+	        if(items.length > 0) alert = true;
+	        setStatus(items.length > 0 ? Status.ALERT.i : Status.RUNNING, items.length);
+	    }
+	    if(alert) {
+	    	setStatus(Status.ALERT);
+	    }
+    }, settings.fRefresh);
+}
+function settingsMenu() {
+	var buttons = [].concat(
+		{ t: "header", text: "Detection" },
+		{ t: "subregion/4:detectionsub" },
+		{ t: "h/12" },
+		createUservarInput("npcs", settings.npcs, { t: "bool", n: "NPC's",  }),
+		createUservarInput("items", settings.items, { t: "bool", n: "Item's" }),
+		createUservarInput("players", settings.players, { t: "bool", n: "Player's" }),
+		{ t: "header", text: "Settings" },
+		{ t: "subregion/10:settingssub" },
+		createUservarInput("draw", settings.draw, { t: "bool", n: "Draw on Map" }),
+		createUservarInput("dSize", settings.dSize, { t: "int", n: "Draw Size" }),
+		createUservarInput("fRefresh", settings.fRefresh, { t: "int", n: "Find Refresh Rate (ms)" }),
+		createUservarInput("mRefresh", settings.mRefresh, { t: "int", n: "Map Find Refresh Rate (ms)" }),
+		{ t: "h/11" },
+		{ t: "button:confirm", text: "Confirm" },
+		{ t: "button:cancel", text: "cancel" }
+	);
+
+	var box = promptbox2({ title: "Settings", style: "popup", width: 300, stylesheets: ["runeappslib.css"] }, buttons);
+	console.log(box);
+	box.cancel.onclick = box.frame.close.b();
+	box.confirm.onclick = function () {
+		settings.npcs = box.npcs.getValue();
+		settings.items = box.items.getValue();
+		settings.players = box.players.getValue();
+		settings.draw = box.draw.getValue();
+		settings.dSize = box.dSize.getValue();
+		settings.fRefresh = box.fRefresh.getValue();
+		settings.mRefresh = box.mRefresh.getValue();
+		updateMainStatus();
+		mapInterval();
+		searchInterval();
+		saveStorage();
+		box.frame.close();
+	}
+}
+
+function updateMainStatus() {
+	var text = '';
+	if(settings.npcs)
+		text += 'NPCS ';
+	else
+		elid(Status.ALERT.n.status).innerText = '';
+	if(settings.items)
+		text += 'ITEMS ';
+	else
+		elid(Status.ALERT.i.status).innerText = '';
+	if(settings.players)
+		text += 'PLAYERS ';
+	else
+		elid(Status.ALERT.p.status).innerText = '';
+
+	setStatusText(text != '' ? 'Search: '+ text : 'Currently not Searching');
+	if(text == '') 
+		setStatus(Status.PAUSED);
+}
+
+function createUservarInput(id, value, meta) {
+	if (["string", "int", "number", "color"].indexOf(meta.t) != -1) {
+		return [
+			{ t: "h/11" },
+			{ t: "text", text: meta.n },
+			{ t: meta.t, id: id, v: value }
+		];
+	}
+	else if (meta.t == "dropdown") {
+		return [
+			{ t: "h/11" },
+			{ t: "text", text: meta.n },
+			{ t: "dropdown", id: id, options: meta.options || meta.getOptions(), v: value }
+		]
+	}
+	else if (meta.t == "bool") { return [{ t: "bool", id: id, v: value, text: meta.n }]; }
+}
+
+function setStatusText(status) {
+	settings.sText = status;
+	setStatus(settings.status);
+}
+function setStatus(status, num) {
+	if(status.status) {
+		elid(status.status).innerText = status.text + ' - ' + num;
+	}
+    if (statusText) {
+        statusText.innerText = settings.sText;
+    }
+    if (container) {
+    	container.className = status.class;
+    }
+    if (stateButton) {
+        var stateButtonText = settings.status.id == Status.PAUSED.id ? "Start" : "Pause";
+        stateButton.innerText = stateButtonText;
+    }
+    settings.status = status;
+}
+
+function find(minimapRegion, dot) {
+    if (!minimap) {
+        return null;
+    }
+    return a1lib.findsubimg(minimapRegion, dot);
+}
+
+function findMinimap() {
+    minimap = minimapReader.find();
+}
+function loadStorage() {
+	//global settings
+	var settingsImport=jsonDecode( localStorage.spotter_settings);
+	console.log(settingsImport);
+	if (settingsImport) {
+		for (var a in settingsImport) {
+			if (typeof settings[a] != "undefined") { settings[a] = settingsImport[a]; }
+		}
+	}
+}
+
+function saveStorage() {
+	//global settings
+	localStorage.spotter_settings = jsonEncode(settings);
+}
